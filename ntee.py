3d0b0f2aa9345894053c3cd32ced1529f2f1a602
Konrad Sperfeld <konrad.sperfeld@uni-rostock.de>
2021-03-23 07:48:16 +0100

    added title

--- ntee.py
+++ ntee.py
@@ -15,12 +15,13 @@ def main():
         "NER Trainer": nertrainer,
         "NER Prediction": nerprediction,
     }
+    st.sidebar.latex('\Huge{NTEE}')
+    st.sidebar.title("NEISS TEI Entity Enricher")
 
     #Include NEISS Logo
     neiss_logo=Image.open('neiss_logo_nn_pentagon01b2.png')
     st.sidebar.image(neiss_logo)
 
-    st.sidebar.title("NEISS TEI Entity Enricher")
 
     #Define sidebar as radiobuttons
     state.page = st.sidebar.radio("Main Menu", tuple(pages.keys()),tuple(pages.keys()).index(state.page) if state.page else 0)
