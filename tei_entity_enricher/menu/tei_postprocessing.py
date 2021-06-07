f47bc3d0a318d0b07816fe06f613da18c0f05acd
marc-lemke <marc.lemke@uni-rostock.de>
2021-06-07 23:33:52 +0200

    Still two bugs: no success message in file_loader, csv file loading doesnt work

--- tei_entity_enricher/menu/tei_postprocessing.py
+++ tei_entity_enricher/menu/tei_postprocessing.py
@@ -99,6 +99,7 @@ class TEINERPostprocessing:
             with el_add_entities_from_file_subcontainer:
                 if self.state.pp_el_object is None:
                     el_add_entities_from_file_loader = None
+                    el_add_entities_from_file_button = st.empty()
                 else:
                     el_add_entities_from_file_loader = st.file_uploader(
                         label="Add entities from file",
@@ -107,9 +108,11 @@ class TEINERPostprocessing:
                         key=None,
                         help="Use json or csv files to add entities to the loaded library. Importing multiple files at once is possible, see the documentation for file structure requirements.",
                     )
-                    if el_add_entities_from_file_loader is not None:
-                        el_add_entities_from_file_button_container = st.empty()
-                        el_add_entities_from_file_success_message = st.empty()
+                    if len(el_add_entities_from_file_loader) > 0:
+                        el_add_entities_from_file_button = st.button(label="Start adding process", key=None, help=None)
+                    else:
+                        el_add_entities_from_file_button = st.empty()
+                el_add_entities_from_file_success_message = st.empty()
         # processes
         if el_init_button == True:
             if self.state.pp_el_object is None:
@@ -143,22 +146,19 @@ class TEINERPostprocessing:
             el_misc_message.empty()
             el_init_message.empty()
             self.reset_el_state_variable("all")
-        if (el_add_entities_from_file_loader is not None) and (len(el_add_entities_from_file_loader) > 0):
-            with el_add_entities_from_file_subcontainer:
-                el_add_entities_from_file_button = el_add_entities_from_file_button_container.button(
-                    label="Start adding process", key=None, help=None
+        if el_add_entities_from_file_button == True:
+            for uploaded_file in el_add_entities_from_file_loader:
+                el_add_entities_from_file_single_file_result = self.state.pp_el_object.add_entities_from_file(
+                    file=uploaded_file
                 )
-            if el_add_entities_from_file_button == True:
-                for uploaded_file in el_add_entities_from_file_loader:
-                    el_add_entities_from_file_single_file_result = self.state.pp_el_object.add_entities_from_file(
-                        file=uploaded_file
+                logger.info(
+                    f"add_entities_from_file() result: {el_add_entities_from_file_single_file_result}. is el_add_entities_from_file_single_file_result[0] == 0?: {el_add_entities_from_file_single_file_result[0] == 0}"
+                )
+                if el_add_entities_from_file_single_file_result[0] == 0:
+                    el_add_entities_from_file_success_message.success(
+                        f"Data from {uploaded_file.name} successfully added to library. But {el_add_entities_from_file_single_file_result[1]} entity/ies has/have been ignored due to redundance issues."
                     )
-                    with el_add_entities_from_file_success_message.beta_container():
-                        if el_add_entities_from_file_single_file_result[0] == 0:
-                            st.success(
-                                f"Data from {uploaded_file.name} successfully added to library. But {el_add_entities_from_file_single_file_result[1]} entity/ies has/have been ignored due to redundance issues."
-                            )
-                        elif type(el_add_entities_from_file_single_file_result) == str:
-                            st.error(el_add_entities_from_file_single_file_result)
+                if type(el_add_entities_from_file_single_file_result) == str:
+                    el_add_entities_from_file_success_message.error(el_add_entities_from_file_single_file_result)
         ## 2. Manual TEI Postprocessing
         tmp.TEIManPP(self.state)
